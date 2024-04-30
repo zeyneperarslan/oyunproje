f01881eef0e2c105900e47b4c3105dae9983b664
Zeynep Erarslan <120674682+zeyneperarslan@users.noreply.github.com>
2024-04-30 18:46:05 +0300

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-let oyun = new Game(document)
+let oyun = new Game(document)  //Game sınıfının özelliklerine ve metodlarına erişim sağlandı , bu özellikler ve metodlar üzerinden oyun işlemleri gerçekleştirildi.
